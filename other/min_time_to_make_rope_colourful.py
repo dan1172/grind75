5b8ba7c052d954f9363e8fefca1c5d17fb3e4ee1
Daniel Khuu <d.khuu@student.unsw.edu.au>
2023-12-28 13:38:26 +1100

    yuh

--- other/min_time_to_make_rope_colourful.py
+++ other/min_time_to_make_rope_colourful.py
@@ -72,6 +72,62 @@ class Solution:
 #print(minCost("baab", [8,7,2,10]))
 
 
-def minCost(colors: str, neededTime):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# new 
+
+# def minCost(colors: str, neededTime):
+#     cur_block, total, max_of_block, sum_of_block = 0, 0 ,0, 0, 0
     
-    return 0 
+#     for i in range(len(colors)):
+#         if colors[i] == colors[cur_block]:
+#             max_of_block = max(max_of_block, neededTime[i])
+    
+#     while cur < len(colors) and p < len(colors):
+#         if colors[p] == colors[cur]:
+#             max_of_block = max(max_of_block, neededTime[p])
+#             sum_of_block += neededTime[p]
+#         else:
+#             total += sum_of_block - max_of_block
+#             max_of_block, sum_of_block = 0, 0
+#             cur = p + 1
+#         p += 1
+#     total += sum_of_block - max_of_block
+#     return total
+
+
+
+
+def minCost(colors: str, neededTime):
+    total_time, most_time, sum_time= 0, neededTime[0], neededTime[0]
+    for i in range(1, len(colors)):
+        if colors[i] != colors[i - 1]:
+            total_time += sum_time - most_time
+            most_time, sum_time = neededTime[i], neededTime[i]
+        else:
+            most_time = max(most_time, neededTime[i])
+            sum_time += neededTime[i]
+    total_time += sum_time - most_time
+    return total_time
+
+#print(minCost("aaaa", [8,7,2,10]))
+#print(minCost("aabaa", [1,2,3,4,1]))
+#print(minCost("baab", [8,7,2,10]))
+print(minCost("cddcdcae", [4,8,8,4,4,5,4,2])) # expected = 8
+
